a19ece8a8433881f84953b54a1e6ecf8f54fe78f
Your Name <you@example.com>
2021-02-10 18:40:36 -0600

    Adding Java Code that reads data.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+// Referencing the Table Body
+let tbody = d3.select("tbody");
+
+// Looping Through tableData
+tableData.forEach((ufoSitings) => {
+    // Appending Table Rows for each ufoSiting object
+    let row = tbody.append("tr");
+    // Using Object.entries to console.log the keys and values
+    Object.entries(ufoSitings).forEach(([key, value]) => {
+        //Appending Cell to Row for Each Value
+        let cell = row.append("td");
+        cell.text(value);
+    });
+});
