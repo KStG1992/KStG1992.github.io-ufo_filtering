94c581b4856f364e05afec0d18296f9eeefb3efd
Your Name <you@example.com>
2021-02-10 20:26:03 -0600

    Adding Code for Filter, currently not working.

--- static/js/app.js
+++ static/js/app.js
@@ -15,3 +15,30 @@ tableData.forEach((ufoSitings) => {
         cell.text(value);
     });
 });
+
+// Select the Filter Search Bar
+let datetime = d3.select("#datetime");
+
+// Select the Button
+let button = d3.select("#filter-btn");
+
+// Creating Event Handler
+datetime.on("submit", runEnter);
+button.on("click", runEnter);
+
+// Complete Event Handler Function
+function runEnter() {
+    
+    // Prevent Page from Refreshing
+    d3.event.preventDefault();
+
+    // Selecting Input Element
+    let inputElement = d3.select("#datetime");
+
+    // Getting Value Property
+    let inputValue = inputElement.property("value");
+    // Filtering Data Based on Input
+    let filterData = tableData.filter(siting => siting.datetime === inputValue);
+    // Console.log filterData
+    console.log(filterData);
+}
