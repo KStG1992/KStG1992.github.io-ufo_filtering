74ea35ed659c38ad853f5b5baed48d4c8efdf6c9
Your Name <you@example.com>
2021-02-13 17:55:09 -0600

    Editing app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,52 @@
-// from data.js
-let tableData = data;
-
-// Referencing the Table Body
-let tbody = d3.select("tbody");
-
-// Looping Through tableData
-tableData.forEach((ufoSitings) => {
-    // Appending Table Rows for each ufoSiting object
-    let row = tbody.append("tr");
-    // Using Object.entries to console.log the keys and values
-    Object.entries(ufoSitings).forEach(([key, value]) => {
-        //Appending Cell to Row for Each Value
-        let cell = row.append("td");
-        cell.text(value);
-    });
-});
-
 // Select the Filter Search Bar
-let datetime = d3.select("#datetime");
-
-// Select the Button
+let input = d3.select("#datetime");
+// Select the Filter Button
 let button = d3.select("#filter-btn");
+// Select the Reset Button
+let reset_button = d3.select("#reset-btn");
 
 // Creating Event Handler
-datetime.on("submit", runEnter);
-button.on("click", runEnter);
+input.on("submit", filterTable);
+button.on("click", filterTable);
+reset_button.on("click", resetTable);
 
-// Complete Event Handler Function
-function runEnter() {
+// Creating Function to Render Data Table
+function renderTable(ufo_data) {
+    // Referencing the Table Body
+    let tbody = d3.select("tbody");
+    // Clearing Table Before Rendering
+    tbody.html("");
     
+    // Looping Through tableData
+    ufo_data.forEach((ufoSightings) => {
+        // Appending Table Rows for each ufoSiting object
+        let row = tbody.append("tr");
+        // Using Object.entries to console.log the keys and values
+        Object.entries(ufoSightings).forEach(([key, value]) => {
+            //Appending Cell to Row for Each Value
+            let cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+// Creating Function to Filter Data Table
+function filterTable() {
     // Prevent Page from Refreshing
     d3.event.preventDefault();
-
-    // Selecting Input Element
-    let inputElement = d3.select("#datetime");
-
     // Getting Value Property
-    let inputValue = inputElement.property("value");
+    let inputValue = input.property("value");
     // Filtering Data Based on Input
-    let filterData = tableData.filter(siting => siting.datetime === inputValue);
-    // Console.log filterData
-    console.log(filterData);
+    let filterData = data.filter(sighting => sighting.datetime === inputValue);
+   // Calling Function to Build Table with Filter Data
+    renderTable(filterData);   
 }
+
+// Creating Function to Reset Filter Data Table and Input
+function resetTable() {
+    renderTable(data);
+    document.getElementById('datetime').value = ''
+}
+
+// Calling Function
+renderTable(data);
